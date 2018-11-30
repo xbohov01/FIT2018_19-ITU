445d4d1127e13dc9f7abedd31cd1d5cd2f9dd8ee
TheSpaceCoder <samo.bohovic@gmail.com>
2018-11-30 15:38:28 +0100

    list complete

--- app/src/main/java/team7/itu2018/TestSelection.java
+++ app/src/main/java/team7/itu2018/TestSelection.java
@@ -1,7 +1,9 @@
 package team7.itu2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TestSelection extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TestSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_selection);
+    }
+
+    public void testSelectButton(View view){
+        Intent intent = new Intent(this, RandomTestActivity.class);
+        startActivity(intent);
     }
 }
